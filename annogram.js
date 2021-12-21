f929aac0901cd1551fb9674a6029600a2af33621
dhowe <dhowe@cims.nyu.edu>
2021-12-21 20:05:35 +0800

    add displayHtml

--- annogram.js
+++ annogram.js
@@ -47,13 +47,13 @@ class Annogram {
 
     let addMeta = (idx) => {
       let sourceId = -1;
-      if (idx === words.length -1 || tokens.length > 1 || !RiTa.isPunct(tokens[0])) { // skip if single punct token
+      if (idx === words.length - 1 || tokens.length > 1 || !RiTa.isPunct(tokens[0])) { // skip if single punct token
         sourceId = this.lookupSource(tokens, { text, index: 0 })[0].id;
         poem.meta.push({ sourceId, tokens, start: (idx - tokens.length) + 1 });
         tokens = [];
       }
       //console.log(`[#${meta.sourceId}]`, RiTa.untokenize(tokens));
-      
+
     }
 
     for (let i = 0; i < words.length; i++) {
@@ -154,6 +154,72 @@ class Annogram {
     return result;
   }
 
+  displayHtml(poem) {
+    let cursor = 0, maxLineWidth = 70;
+    let resultDiv = html`<div class="display"></div>`;
+
+    for (let i = 0; i < poem.meta.length; i++) {
+      let m = poem.meta[i];
+
+      // Note that some meta elements may have id = -1
+      //   which means they shouldn't get a highlight
+      if (m.sourceId < 0) throw Error('TODO: handle sourceId == -1');
+
+      let toks = m.tokens.slice(cursor - m.start);
+      let src = this.source.find(p => p.id === m.sourceId);
+      if (!src) throw Error('No source for sourceId #' + m.sourceId);
+
+      let next = RiTa.untokenize(toks);
+      if (raw.length && !RiTa.isPunct(next[0])) resultDiv.append(' ');
+
+      let sourceDiv = html`<div class="source" id="source${i}"></div>`;
+      let regexStr = next.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+      if (/[A-Za-z]/.test(next[0])) regexStr = "(?<![A-Za-z])" + regexStr;
+      if (/[A-Za-z]/.test(next[next.length - 1])) regexStr += "(?![A-Za-z])";
+
+      const regex = new RegExp(regexStr);
+      let inOriginIndexFrom = (regex.exec(src.text)) ? (regex.exec(src.text)).index : src.text.indexOf(next);
+      let inOriginIndexTo = inOriginIndexFrom + next.length;
+      let before = "", beforeStartIndex = inOriginIndexFrom - 1;
+
+      while (beforeStartIndex >= 0 && !/[.?!]/.test(src.text[beforeStartIndex])) {
+        if (src.text[beforeStartIndex] === ' ' && before.length > maxLineWidth) {
+          before = "... " + before;
+          break;
+        }
+        before = src.text[beforeStartIndex] + before;
+        beforeStartIndex--;
+      }
+
+      let after = "", afterStartIndex = inOriginIndexTo;
+      while (src.text[afterStartIndex] && !/[.?!]/.test(src.text[afterStartIndex])) {
+        if (src.text[afterStartIndex] === ' ' && after.length > maxLineWidth) {
+          break;
+        }
+        after += src.text[afterStartIndex];
+        afterStartIndex++;
+      }
+      after += after.length > 70 ? " ..." : src.text[afterStartIndex];
+
+      sourceDiv.append(html`<span class="sourceText">${before}</span>`);
+      sourceDiv.append(html`<span class="sourceHighlight">${next}</span>`);
+      sourceDiv.append(html`<span class="sourceText">${after}</span>`);
+
+      // handle titles starting with 'from'
+      let title = src.title.trim().replace(/^[Ff]rom /, '');
+      sourceDiv.append(
+        html`<p class="sourceFootnote">from <i>${title}</i> by ${src.author}</p>`
+      );
+
+      let thisSegment = html`<a href="javascript:void(0)" class="meta">${next}</a>`;
+      thisSegment.append(sourceDiv);
+      resultDiv.append(thisSegment);
+
+      cursor += toks.length;
+    }
+
+    return resultDiv;
+  }
 }
 
 Annogram.lb = '<p>';
