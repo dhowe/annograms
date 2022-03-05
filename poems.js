c4c495e9869e82802d9e9f7dc78ab77c03321a91
dhowe <dhowe@cims.nyu.edu>
2022-03-05 20:10:53 +0800

    use cdn for rita link

--- poems.js
+++ poems.js
@@ -6,6 +6,7 @@ function loadPoems() {
     require('./data/Great_American_Prose_Poems.json'),
     require('./data/Short_An_International_Anthology.json'),
     require('./data/Anthology_of_Australian_Prose_Poetry.json'),
+    require('./data/Modern_Poetry.json'),
   ).map((p, i) => {
     let text = p.text
       .replace(/[”“()]/g, '')
@@ -16,11 +17,12 @@ function loadPoems() {
       .replace(/:/, ',');
     //let minSentenceLength = 7;
     let sents = RiTa.sentences(text);
-    sents.map(s => {
+    sents = sents.map(s => {
       let words = RiTa.tokenize(s);
       words.map(w => w.startsWith('I') ? w : w.toLowerCase());
       return RiTa.capitalize(RiTa.untokenize(words));
     });
+    require('fs').writeFileSync('sentences.json', JSON.stringify(sents,0,2));
     p.id = i;
     p.text = sents.join(' ');
     return p;
@@ -29,4 +31,9 @@ function loadPoems() {
 }
 
 let poems = loadPoems();
+if (0) {
+  let fname = "poems-html.json";
+  require('fs').writeFileSync(fname, JSON.stringify(poems));
+  console.log('Wrote '+poems.length+' poems to '+fname);
+}
 export default poems;
